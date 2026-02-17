a1a6cbcc371c2a385625881012c879d41e4501c7
SHW378 <cesarbrmn@hotmail.com>
2026-02-17 15:23:12 -0600

    ejercicio 1 VAriables

---
+++ app/lib/src/main/java/Ejercicios/1-Variables.kt
@@ -0,0 +1,13 @@
+package Ejercicios
+/*
+* Ejercicio 1 - Variables (Ejercicio01_Variables.kt)
+Usa string templates para imprimir un mensaje con los datos de las variables que ya estan declaradas.
+* */
+fun main() {
+    val nombre: String = "Carlos"
+    val edad: Int = 21
+    val estatura: Double = 1.78
+    val esEstudiante: Boolean = true
+
+    println("Me llamo Carlos, tengo $edad años, mido $estatura y es $esEstudiante que soy estudiante")
+}
